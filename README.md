3c45ce64f54c2660923b6b326d64617d20cb0670
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-14 21:06:27 +0800

    remove all

--- README.md
+++
@@ -1,3 +0,0 @@
-# crud_app
-Data Encoding and Reportting App
-URL Deployment: https://www.booleansolutionsenterprises.com/crud_app/
--- script/classes/templates/bootstrap4/template-filter.php
+++
@@ -1,8 +0,0 @@
-<section class="card">
-    <header class="card-heading">
-        <?php echo $displayText; ?>
-    </header>
-    <div class="card-body">
-        <?php echo $filterControl; ?>
-    </div>
-</section>
