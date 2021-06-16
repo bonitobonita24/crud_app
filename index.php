b492c35ac1d096e65631dc87f4996d00494cff1e
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-16 22:00:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require "script/pdocrud.php";
 $pdocrud = new PDOCrud();
 $pdocrud->formDisplayInPopup();
 $pdocrud->setSearchCols(array("user","email","role"));
-$pdocrud->setSettings("printBtn","false");
-$pdocrud->setSettings("pdfBtn","false");
+$pdocrud->setSettings("printBtn",false);
+$pdocrud->setSettings("pdfBtn",false);
 echo $pdocrud->dbTable("users")->render();
 
 
