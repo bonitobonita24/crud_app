af8c96d070b617d6eba712c3b989be30521321fd
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-16 22:57:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $pdocrud->crudRemoveCol(array("user_id"));
 $pdocrud->fieldTypes("photo", "FILE_NEW");
 $pdocrud->tableColFormatting("photo", "image", array("width"=>"50px"));
 $pdocrud->viewColFormatting("photo","image", array("width"=>"35%"));
-$pdocrud->setViewColumns(array("user", "pass", "email", "role"));
+$pdocrud->setViewColumns(array("user", "pass", "email", "role","photo"));
 echo $pdocrud->dbTable("users")->render();
 ?>
