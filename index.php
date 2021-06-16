07a2851ac800d37c22520cb74dbe76e1c5383f1e
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-16 22:31:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require "script/pdocrud.php";
 $pdocrud = new PDOCrud();
 $pdocrud->formDisplayInPopup();
 $pdocrud->setSearchCols(array("user","email","role"));
-$pdocrud->fieldGroups("Name1",array("user","pass"));
+$pdocrud->fieldGroups("Name",array("user","pass"));
 $pdocrud->fieldGroups("Name2",array("email","role"));
+$pdocrud->fieldGroups("Name3",array("photo"));
 $pdocrud->crudRemoveCol(array("user_id"));
 $pdocrud->fieldTypes("photo", "FILE_NEW");
 
