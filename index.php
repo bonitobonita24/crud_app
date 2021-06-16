7088d7fa29db77369e944eed0533422aaa6a9ae6
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-16 21:53:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require "script/pdocrud.php";
 
 $pdocrud = new PDOCrud();
 $pdocrud->formDisplayInPopup();
+$pdocrud->setSearchCols(array("user","email","role"));
 echo $pdocrud->dbTable("users")->render();
 
 
