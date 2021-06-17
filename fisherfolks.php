ffeb70270b8315ed67b1095586f9da8c3777b67b
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-17 15:33:41 +0800

    Create fisherfolks.php

---
+++ fisherfolks.php
@@ -0,0 +1,14 @@
+<?php
+require "script/pdocrud.php";
+
+$pdocrud = new PDOCrud();
+$pdocrud->formDisplayInPopup();
+$pdocrud->setSearchCols(array("id_number","last_name","first_name","address","batch_code","modified_by","last_update"));
+$pdocrud->crudRemoveCol(array("fisherfolks_id"));
+$pdocrud->fieldTypes("image", "FILE_NEW");
+$pdocrud->fieldTypes("signature", "FILE_NEW");
+// $pdocrud->tableColFormatting("photo", "image", array("width"=>"50px"));
+// $pdocrud->viewColFormatting("photo","image", array("width"=>"35%"));
+// $pdocrud->setViewColumns(array("user", "pass", "email", "role","photo"));
+echo $pdocrud->dbTable("users")->render();
+?>
