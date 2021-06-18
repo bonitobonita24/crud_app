fa190de96d799d8f7027fe2c2f7ef1d669a0fd17
Bonito Bonita <bonitobonita24@gmail.com>
2021-06-18 12:43:22 +0800

    Update fisherfolks.php

--- fisherfolks.php
+++ fisherfolks.php
@@ -2,7 +2,7 @@
 require "script/pdocrud.php";
 $pdocrud = new PDOCrud(false, "", "", array("autoSuggestion" => true, "showAllSearch" => false));
 $pdocrud->dbOrderBy("last_update desc");
-$pdocrud->formDisplayInPopup();
+// $pdocrud->formDisplayInPopup();
 $pdocrud->setSearchCols(array("id_number","last_name","first_name","address","batch_code","modified_by","last_update"));
 $pdocrud->crudRemoveCol(array("fisherfolks_id"));
 $pdocrud->fieldTypes("image", "FILE_NEW");
