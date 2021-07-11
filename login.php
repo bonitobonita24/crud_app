b4618d72d5c9bca31eb1161d3fb6a2e875726889
Bonito Bonita <bonitobonita24@gmail.com>
2021-07-11 22:34:54 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require "script/pdocrud.php";
 $pdocrud = new PDOCrud();
 
-$pdocrud->formFields(array("email","password"));
+$pdocrud->formFields(array("user","pass"));
 echo $pdocrud->dbTable("users")->render();
 
 
