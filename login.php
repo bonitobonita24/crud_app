34fa0edbac166892840c85e1f4eee7c4cb777598
Bonito Bonita <bonitobonita24@gmail.com>
2021-07-24 18:04:15 +0800

    Update login.php

--- login.php
+++ login.php
@@ -62,7 +62,7 @@
                                     //Login page code
                                     $pdo_crud = new PDOCrud();
                                     // (Optional step) before checking the login data if you want to peform any operation like password encryption etc/(optional)
-                                     $pdo_crud->addCallback("before_select", "beforeloginCallback");
+                                    //  $pdo_crud->addCallback("before_select", "beforeloginCallback");
                                      //only required fields to be display on form
                                      $pdo_crud->formFields(array("email", "password"));
                                      //set session variables - 1st parameter is the session variable name and 2nd is value to be matched in database table
@@ -73,7 +73,7 @@
                                      $pdo_crud->setUserSession("role", "role");
                                      
                                     //  $pdo_crud->setUserSession("lastLoginTime", date("now"));
-                                    $pdo_crud->setUserSession("last_login_date", date("now"));
+                                    $pdo_crud->setUserSession("lastLoginTime", date("now"));
                                     ///redirect to some page after login (optional)-2nd parameter specify whether to redirect or not if no records found
                                      $pdo_crud->formRedirection("https://www.booleansolutionsenterprises.com/crud_app/after-login-page.php", true);
                                     // set db table to your user table and call render function with select form
